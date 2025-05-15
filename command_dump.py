28d661f66a8ff595d234e0280024a5c9893d82a0
Spencer Murray <spencer.murray@sentry.io>
2025-05-15 08:03:55 -0400

    Check for executables in command_dump.py

--- command_dump.py
+++ command_dump.py
@@ -31,5 +31,23 @@ def command_dump(commands: list[str], out_path: str):
 
 
 if __name__ == "__main__":
+    try:
+        subprocess.check_call(["codecovcli", "--help"], stdout=subprocess.DEVNULL)
+    except Exception:
+        print(
+            "codecovcli is not executable. You can install it with pip install -e codecov-cli"
+        )
+        exit(1)
+
+    try:
+        subprocess.check_call(
+            ["sentry-prevent-cli", "--help"], stdout=subprocess.DEVNULL
+        )
+    except Exception:
+        print(
+            "sentry-prevent-cli is not executable. You can install it with pip install -e prevent-cli"
+        )
+        exit(1)
+
     command_dump(["codecovcli"], "codecov-cli/codecovcli_commands")
     command_dump(["sentry-prevent-cli"], "prevent-cli/preventcli_commands")
