9da73c38f0cb1e14f12a23bc408b0a537e1588e6
Spencer Murray <spencer.murray@sentry.io>
2025-05-28 15:34:29 -0400

    Fix alpine

--- codecov-cli/scripts/build_alpine.sh
+++ codecov-cli/scripts/build_alpine.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apk add build-base python3 py3-pip
 cd codecov-cli
-pip install uv
+pip install uv --break-system-packages
 uv sync
 uv add --dev pyinstaller
 uv run pyinstaller -F codecov_cli/main.py
