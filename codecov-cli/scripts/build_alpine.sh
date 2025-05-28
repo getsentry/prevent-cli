174e674e6b814b370650c898610ed85d9013f0ca
Spencer Murray <spencer.murray@sentry.io>
2025-05-28 15:29:19 -0400

    Fix build?

--- codecov-cli/scripts/build_alpine.sh
+++ codecov-cli/scripts/build_alpine.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-apk add build-base
+apk add build-base python3 py3-pip
 cd codecov-cli
 pip install uv
 uv sync
 uv add --dev pyinstaller
 uv run pyinstaller -F codecov_cli/main.py
-cp ./dist/main ./dist/codecovcli_$1
+mv ./dist/main ./dist/codecovcli_$1
---
+++ codecov-cli/scripts/build_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt install build-essential python3 python3-pip
+cd codecov-cli
+pip install uv
+uv sync
+uv add --dev pyinstaller
+uv run pyinstaller -F codecov_cli/main.py
+mv ./dist/main ./dist/codecovcli_$1
+
+# linux binary should be just codecovcli_linux
+if [ $1 = "linux_x86_64" ]; then
+    mv ./dist/codecovcli_$1 ./dist/codecovcli_linux
+fi
