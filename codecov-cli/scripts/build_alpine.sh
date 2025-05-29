0cdb0b7b76ed6bb6f287d6447f0d4479b8cb8587
Spencer Murray <spencer.murray@sentry.io>
2025-05-29 14:27:04 -0400

    Download uv binary instead of building from source with pip

--- codecov-cli/scripts/build_alpine.sh
+++ codecov-cli/scripts/build_alpine.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 apk add build-base python3 py3-pip
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.7.8/uv-installer.sh
+# Added sanity check file has not been tampered with
+if [[ $(sha256sum uv-installer.sh) == "3e3043ca08e1156fbe18d90a1a4def3ae795418857c8f4ed3f807ffc45e51c3d  uv-installer.sh" ]]; then
+    
+    sh uv-installer.sh
+fi
 cd codecov-cli
-python3.9 -m pip install uv
-uv python pin 3.9
-uv sync
-uv add --dev pyinstaller
-uv run pyinstaller -F codecov_cli/main.py
+/root/.local/bin/uv python pin 3.9
+/root/.local/bin/uv sync
+/root/.local/bin/uv add --dev pyinstaller
+/root/.local/bin/uv run pyinstaller -F codecov_cli/main.py
 mv ./dist/main ./dist/codecovcli_$1
