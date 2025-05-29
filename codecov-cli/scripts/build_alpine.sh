85f75e7e1fabacbe4d0ba8f65f66a51d318a9ec4
Spencer Murray <spencer.murray@sentry.io>
2025-05-29 14:27:50 -0400

    curl

--- codecov-cli/scripts/build_alpine.sh
+++ codecov-cli/scripts/build_alpine.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apk add build-base python3 py3-pip
+apk add build-base python3 py3-pip curl
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.7.8/uv-installer.sh
 # Added sanity check file has not been tampered with
 if [[ $(sha256sum uv-installer.sh) == "3e3043ca08e1156fbe18d90a1a4def3ae795418857c8f4ed3f807ffc45e51c3d  uv-installer.sh" ]]; then
