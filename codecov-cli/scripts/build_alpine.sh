36444d9a465f9372184be03a29a3edaccd419767
Spencer Murray <spencer.murray@sentry.io>
2025-05-29 14:30:17 -0400

    idk

--- codecov-cli/scripts/build_alpine.sh
+++ codecov-cli/scripts/build_alpine.sh
@@ -4,7 +4,8 @@ curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/d
 # Added sanity check file has not been tampered with
 if [[ $(sha256sum uv-installer.sh) == "3e3043ca08e1156fbe18d90a1a4def3ae795418857c8f4ed3f807ffc45e51c3d  uv-installer.sh" ]]; then
     
-    sh uv-installer.sh
+    chmod +x uv-installer.sh
+    ./uv-installer.sh
 fi
 cd codecov-cli
 /root/.local/bin/uv python pin 3.9
