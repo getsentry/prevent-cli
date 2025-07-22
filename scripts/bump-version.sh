c01157861e9d84c152f7e658a7758c3fc6aab2d1
Spencer Murray <spencer.murray@sentry.io>
2025-07-22 14:40:55 -0400

    Final tweaks before test run

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,7 +3,12 @@ set -eux
 # $1 is the old version but we don't need it
 NEW_VERSION="${2}" 
 
-sed -i "s/version\ =\ \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/version\ =\ \"$NEW_VERSION\"/g" codecov-cli/pyproject.toml
+# update package versions
+sed -i "s/version\ =\ \"[0-9]\+.[0-9]\+.[0-9]\+\"/version\ =\ \"$NEW_VERSION\"/g" codecov-cli/pyproject.toml prevent-cli/pyproject.toml
+# update codecov-cli dependency version in prevent-cli
+sed -i "s/codecov-cli==[0-9]\+.[0-9]\+.[0-9]\+/codecov-cli==$NEW_VERSION/g" prevent-cli/pyproject.toml
+
 pip install uv
-uv sync --project codecov-cli # updates uv.lock
+# updates uv.locks
+uv sync --project codecov-cli 
 uv sync --project prevent-cli
