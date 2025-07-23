7d3c357e2df9d6e956ea9a210e866c0990fef0ea
Spencer Murray <spencer.murray@sentry.io>
2025-07-23 09:33:31 -0400

    feat: Build out Craft release flow (#50)
    Builds the release flow for prevent cli using
    [Craft](https://github.com/getsentry/craft) via
    [`action-prepare-release`](https://github.com/getsentry/action-prepare-release)
    and the [getsentry/publish](https://github.com/getsentry/publish) repo
    for release management.
    
    This is the resulting flow:
    
    ### On workflow dispatch with version number:
    - commits result of `scripts/bump_version.sh` to new `release/$version`
    - opens issue on getsentry/publish 
      (example: https://github.com/getsentry/publish/issues/5871)
    
    ### On creation of that release/* branch: 
    - build assets and upload as workflow artifacts (not release artifacts
    yet)
    
    ### On 'accepted' label applied in publish repo issue:
    IF assets are all present and checks are passing:
    - craft merges release branch into main
    - craft publishes prevent-cli to release targets (github release, pypi)
    - a separate workflow publishes codecov-cli like we do today (no craft)
    (github release, pypi, gcs)

--- scripts/build_alpine.sh
+++ scripts/build_alpine.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+set -eux
 apk add build-base python3 py3-pip curl
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.7.8/uv-installer.sh > uv-installer.sh
 # Added sanity check file has not been tampered with
-if sha256sum -c ./codecov-cli/scripts/uv-installer-0.7.8.sha256sum; then
-    sh uv-installer.sh
+if sha256sum -c ./scripts/uv-installer-0.7.8.sha256sum; then
+    sh uv-installer.sh; else
+    echo "uv-installer failed checksum" && exit 1
 fi
-cd codecov-cli
+cd prevent-cli
 /root/.local/bin/uv python pin 3.9 # we need to build with python 3.9 to support systems with libpython >= 3.9
 /root/.local/bin/uv sync
-/root/.local/bin/uv add --dev pyinstaller
+/root/.local/bin/uv run pyinstaller -F src/prevent_cli/main.py
+mv ./dist/main ./dist/sentry-prevent-cli_$1
+
+cd ../codecov-cli
 /root/.local/bin/uv run pyinstaller -F codecov_cli/main.py
 mv ./dist/main ./dist/codecovcli_$1
--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
+set -eux
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y tzdata
 apt install -y python3.9 python3.9-dev python3-pip
-cd codecov-cli
+cd prevent-cli
 python3.9 -m pip install uv --only-binary uv
 uv python pin 3.9 # we need to build with python 3.9 to support systems with libpython >= 3.9
 uv sync
-uv add --dev pyinstaller
+uv run pyinstaller -F src/prevent_cli/main.py
+mv ./dist/main ./dist/sentry-prevent-cli_$1
+cd ../codecov-cli
 uv run pyinstaller -F codecov_cli/main.py
 mv ./dist/main ./dist/codecovcli_$1
 
 # linux binary should be just codecovcli_linux
 if [ $1 = "linux_x86_64" ]; then
-    mv ./dist/codecovcli_$1 ./dist/codecovcli_linux
+    cd ..
+    mv ./prevent-cli/dist/sentry-prevent-cli_$1 ./prevent-cli/dist/sentry-prevent-cli_linux
+    mv ./codecov-cli/dist/codecovcli_$1 ./codecov-cli/dist/codecovcli_linux
 fi
