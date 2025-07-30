a7aa60aafd400a69837822f9db0f82cd5de2e298
Spencer Murray <spencer.murray@sentry.io>
2025-07-30 10:55:27 -0400

    Tweak script for macos compat

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # this updates the editable property on the codecov-cli import in prevent-cli 
 # to be false. This is necessary for the pyinstaller build to work.
-sed -i "s/editable\ =\ true/editable\ =\ false/g" prevent-cli/pyproject.toml
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/editable\ =\ true/editable\ =\ false/g" prevent-cli/pyproject.toml
+else
+    sed -i "s/editable\ =\ true/editable\ =\ false/g" prevent-cli/pyproject.toml
+fi
